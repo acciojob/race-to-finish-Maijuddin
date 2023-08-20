78929aae512d1b1f333d0b2bb9da7e78e4a996c3
Maijuddin Mallick <124431155+Maijuddin@users.noreply.github.com>
2023-08-20 09:38:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-const promises = [
-    new Promise(resolve => setTimeout(() => resolve("Promise 1"), Math.random() * 4000 + 1000)),
-    new Promise(resolve => setTimeout(() => resolve("Promise 2"), Math.random() * 4000 + 1000)),
-    new Promise(resolve => setTimeout(() => resolve("Promise 3"), Math.random() * 4000 + 1000)),
-    new Promise(resolve => setTimeout(() => resolve("Promise 4"), Math.random() * 4000 + 1000)),
-    new Promise(resolve => setTimeout(() => resolve("Promise 5"), Math.random() * 4000 + 1000))
+function createRandomPromise() {
+  const randomTime = Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds in milliseconds
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Promise resolved after ${Math.floor(randomTime / 1000)} seconds`);
+    }, randomTime);
+  });
+}
+
+window.promises = [
+  createRandomPromise(),
+  createRandomPromise(),
+  createRandomPromise(),
+  createRandomPromise(),
+  createRandomPromise()
 ];
 
-// Using Promise.any() to wait for the first resolved promise
-Promise.any(promises)
-    .then(result => {
-        const outputDiv = document.getElementById("output");
-        outputDiv.textContent = result;
-    })
-    .catch(error => {
-        console.error("An error occurred:", error);
-    });
+Promise.any(window.promises)
+  .then((result) => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerText = result;
+  })
+  .catch((error) => console.error("Error:", error));
